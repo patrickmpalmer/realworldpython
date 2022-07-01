6f302b096e74b824fab2d7b64275e6f6e0b0366a
patrickmpalmer <patrickpalmer271@gmail.com>
2022-07-01 14:51:49 -0500

    Bayesian serach game in progress

---
+++ ch1-shipwrecked/bayes.py
@@ -0,0 +1,26 @@
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
+MAP_FILE = 'cape_python.png'
+
+# Assign search area (SA) corner point locations based on image pixels.
+SA1_CORNERS = (130, 265, 180, 315)  # (UL-X, UL-Y, LR-X, LR-Y)
+SA2_CORNERS = (80, 255, 130, 305)   # (UL-X, UL-Y, LR-X, LR-Y)
+SA3_CORNERS = (105, 205, 155, 255)  # (UL-X, UL-Y, LR-X, LR-Y)
+
+class Search():
+    """Bayesian Search & Rescue game with 3 search areas."""
+
+    def __init__(self, name):
+        self.name = name
+        self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
+        if self.img is None:
+            print('Could not load map file {}'.format(MAP_FILE),
+                file=sys.stderr)
+            sys.exit(1)
+
+        self.area_actual = 0
+        
